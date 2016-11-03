5943d0ab837724dac26d577216d4e55155e875e2
hendleje <jenny2912@gmx.de>
2016-11-03 13:42:48 -0230

    created person and customer.js

--- applicationLayer/customer.js
+++ applicationLayer/customer.js
@@ -1,3 +1,31 @@
-/**
- * http://usejsdoc.org/
- */
+var Customer = function(firstName, lastName, email, address) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.address = address;
+	this.id = 1;
+	this.status = "suspended";
+	this.sendConfirmationEmail();
+};
+
+Customer.prototype.checkPreviousBill = function(){
+	return true;
+};
+
+Customer.prototype.sendConfirmationEmail = function() {
+	console.log("Send mail to: " + this.email);
+}
+
+Customer.prototype.customerVerified = function() {
+	this.status = "active";
+	console.log("Customer status: " + this.status);
+}
+
+Customer.prototype.sayHello = function(name) {
+	console.log("Hello, I'm " + this.firstName + " " + this.id);
+}
+
+var customer1 = new Customer ("Jenny", "Hendler", "bla", "bla");
+customer1.sayHello("Hello");
+customer1.checkPreviousBill();
+customer1.customerVerified();
---
+++ applicationLayer/person.js
@@ -0,0 +1,25 @@
+//module.exports = function Person() {}
+//
+//var Person = function(firstName, lastName, email, address) {
+//	this.firstName = firstName;
+//	this.lastName = lastName;
+//	this.email = email;
+//	this.address = address;
+//	this.id = 1;
+//};
+//
+//Person.prototype.sayHello = function(name) {
+//	console.log("Hello, I'm " + this.firstName + ", " +name);
+//}
+//
+//var person1 = new Person("Jenny", "Hendler", "bla", "bla");
+//person1.firstName = "Sandra";
+//person1.sayHello(person1);
+
+//function Person(firstName, lastName, email, address) {
+//	Person.prototype.firstName = firstName;
+//	Person.prototype.lastName = lastName;
+//	Person.prototype.email = email;
+//	Person.prototype.address = address;
+//	Person.prototype.id = "";
+//}
