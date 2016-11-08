65bac51abb9ffe4992b227223c4271b559aa0d82
hendleje <jenny2912@gmx.de>
2016-11-08 12:50:08 -0330

    Make app.js file working
    install bodyparser
    install express
    change app.js

--- app.js
+++ app.js
@@ -1,38 +1,69 @@
 var http = require("http");
 var express = require ("express");
 var app = express();
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var routes = require('routes');
 
-// let us initiate basic handler and add router explicitly
-
-app.use(express.bodyParser());	
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.bodyParser());
 app.use(app.router);
+app.use(express.static(path.join(__dirname, 'presentationLayer')));
+app.use(express.static(path.join(__dirname, 'applicationLayer')));
 
-// basic routes def start here
+app.get("/index.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"index.html");
+});
 
-// when a broswer places a GET req / the response will be followed
-
-app.get("/",function(req,res){	
-	res.send("i am from / GET req");
+app.get("/addCar.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"addCar.html");
 });
 
 
-//let us construct basic form
-
-app.get("/addCar",function(req,res){
-	res.sendfile("./addCar.html"); 
-
-})
-
-app.post("/addCar",function(req,res)
-{
-	var carPlate=req.body.carPlate; 
-	var carBrand=req.body.carBrand;
-	var carModel=req.body.carModel; 
-	var carYear=req.body.carYear; 
-	var carColor=req.body.carColor; 
-	var carType=req.body.carType;  
-	res.send("A new car has been created:</br> Car Plate: "	+carPlate+"</br>Car Brand:"+carBrand+"</br>Car Model:"+carModel+"</br>Year:"+carYear+"</br>Car Color:"+carColor+"</br>Car Type:"+carType);
+app.get("/signIn.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"signIn.html");
 });
 
+app.get("/carDetails.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"carDetails.html");
+});
 
-http.createServer(app).listen(3000);
+app.get("/chooseDirection.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"chooseDirection.html");
+});
+
+app.get("/directionToCar.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"directionToCar.html");
+});
+
+app.get("/directionToLocation.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"directionToLocation.html");
+});
+
+app.get("/emailSent.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"emailSent.html");
+});
+
+app.get("/findCar.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"findCar.html");
+});
+
+app.get("/payBill.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"payBill.html");
+});
+
+app.get("/registerNewUser.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"registerNewUser.html");
+});
+
+app.get("/searchResults.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"searchResults.html");
+});
+
+app.get("/tripDetails.html", function (req, res) {
+	res.sendfile(__dirname+"/presentationLayer/"+"tripDetails.html");
+});
+
+http.createServer(app).listen(8688);
